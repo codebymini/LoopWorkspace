aea8713e3363ee5dd46df7153171c484f883a650
marionbarker <marionbarker@earthlink.net>
2025-08-31 11:05:07 -0700

    wip for LoopWorkspace pr script

--- Scripts/manual_LoopWorkspace_prepare_pr.sh
+++ Scripts/manual_LoopWorkspace_prepare_pr.sh
@@ -23,15 +23,24 @@ echo "3. The commit message in ${message_file} will be used"
 cat ${message_file}
 echo "4. Once the PR is prepared, additional commits can be added as needed"
 
-echo ""
-echo "After completing this process, merging the PR and trimming the ${translation_dir} branch,"
-echo "  be sure to run the export and upload scripts again from the updated dev branch"
+echo "Enter y to proceed, any other character exits"
+read query
 
-if git switch "${translation_dir}"; then
-    echo "The branch ${translation_dir} exists"
+if [[ ${query} == "y" ]]; then
+
+    if git switch "${translation_dir}"; then
+        echo "The branch ${translation_dir} exists"
+    else
+        echo "The branch ${translation_dir} does not exist; it will be created from the current path"
+        git switch -c "${translation_dir}"
+    fi
+
+    ./Scripts/update_submodule_refs.sh
+
+    echo ""
+    echo "After completing this process, merging the PR and trimming the ${translation_dir} branch,"
+    echo "  be sure to run the export and upload scripts again from the updated dev branch"
+
 else
-    echo "The branch ${translation_dir} does not exist; it will be created from the current path"
-    git switch -c "${translation_dir}"
+    echo "user opted to exit the script"
 fi
-
-./Scripts/update_submodule_refs.sh
